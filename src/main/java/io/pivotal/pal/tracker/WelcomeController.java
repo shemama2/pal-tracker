822b87b428cc484b7a9dcd85cdc9665d54d44bce
Hemamalini <shemama2@ford.com>
2019-07-23 14:11:14 +0530

    travis changes 2

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,5 +1,7 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,8 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class WelcomeController {
 
 
+    private String message;
+
+    @Autowired
+    public WelcomeController(@Value("${welcome.message}") String message) {
+        this.message = message;
+    }
+
     @GetMapping("/")
     public String sayHello() {
-        return "hello";
+        return message;
     }
 }
